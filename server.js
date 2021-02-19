701811989c5811acc6ab6d5993e515b5295bf044
Hankscr459 <defcw58@gmail.com>
2021-02-19 15:03:42 +0800

    Adding Resolvers and Executing Queries in GraphQL Playground (Optional)

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer, gql } from 'apollo-server'
 
 
 const todos = [
     { task: 'Wash Car', completed: false },
     { task: 'Clean room', completed: true }
-];
+]
 
 const typeDefs = gql`
-    type Query {
+    type Todo {
         task: String
         completed: Boolean
     }
-`;
+
+    type Query {
+        getTodos: [Todo]
+    }
+`
+
+const resolvers = {
+    Query: {
+        getTodos: () => todos
+    }
+}
 
 const server = new ApolloServer({
-    typeDefs: typeDefs
-});
+    typeDefs: 
+        typeDefs, 
+        resolvers
+})
 
 server.listen().then(({url}) => {
     console.log(`Server listening ${url}`)
-});
+})
