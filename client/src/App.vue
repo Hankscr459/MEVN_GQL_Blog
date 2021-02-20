b387bb3db819b895e089a00a13e0e81371debfa9
Hankscr459 <defcw58@gmail.com>
2021-02-20 19:57:59 +0800

    Installing Vuetify Plugin and Generating a Theme

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <div id="app">
-    <h1>App</h1>
-    <router-view/>
-  </div>
+  <v-app>
+    <div id="app">
+      <h1>App</h1>
+      <v-btn color='primary'>Button</v-btn>
+      <router-view/>
+    </div>
+  </v-app>
 </template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  }
+}
+</script>
