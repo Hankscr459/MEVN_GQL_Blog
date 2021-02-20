3c0534cb4e205f6a1593820047bea22963c1722a
Hankscr459 <defcw58@gmail.com>
2021-02-20 20:42:50 +0800

    Horizontal Navbar and Mobile First Design

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+// import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
@@ -13,3 +14,15 @@ Vue.use(Vuetify, {
     success: '#1B5E20'
   }
 })
+
+// Vue.use(Vuetify, {
+//   theme: {
+//     primary: colors.teal.lighten1,
+//     secondary: colors.teal.lighten3,
+//     accent: colors.red.lighten1,
+//     error: colors.red.accent1,
+//     warning: colors.amber.base,
+//     info: colors.blue.accent3,
+//     success: colors.green.darken4
+//   }
+// })
