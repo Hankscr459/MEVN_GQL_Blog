8e241674198ec18484b2f68db46f9d11751610f4
Hankscr459 <defcw58@gmail.com>
2021-02-21 21:24:39 +0800

    Add Carousel Component to Home Page

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,20 +1,12 @@
 <template>
-    <v-container>
-      <h1>Home</h1>
-      <ApolloQuery :query="getPostsQuery">
-        <template slot-scope="{ isLoading, result: { data, error, networkStatus } }">
-          <div v-if="isLoading">Loading...</div>
-          <div v-else-if="error">Error! {{error.message}}</div>
-          <div v-else-if="networkStatus">Netword Status: {{networkStatus}}</div>
-          <ul v-else v-for="post in data.getPosts" :key="post._id">
-            <li>
-              {{post.title}}
-              {{post.imageUrl}}
-              {{post.likes}}
-            </li>
-          </ul>
-        </template>
-      </ApolloQuery>
+    <v-container text-xs-center v-if="getPosts">
+      <v-flex xs12>
+        <v-carousel v-bind="{'cycle': true}" interval="3000">
+          <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+            <h1 id="carousel__title">{{post.title}}</h1>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
     </v-container>
 </template>
 <script>
@@ -22,9 +14,9 @@ import { gql } from 'apollo-boost'
 
 export default {
   name: 'Home',
-  data () {
-    return {
-      getPostsQuery: gql`
+  apollo: {
+    getPosts: {
+      query: gql`
         query {
           getPosts {
             _id
@@ -39,3 +31,17 @@ export default {
   }
 }
 </script>
+
+<style>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, .5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
