98fcc7a90cfc5db665cf4677c680c5d189e95b6e
Hankscr459 <defcw58@gmail.com>
2021-02-22 11:59:14 +0800

    Create Gravatar Avatar and Hash User Passwords on Signup

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 import  mongoose from 'mongoose'
+import md5 from 'md5'
+import bcrypt from 'bcrypt'
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -30,6 +32,32 @@ const UserSchema = new mongoose.Schema({
     ref: "Post"
   }
 });
+
+
+// Create and add avatar to user
+UserSchema.pre('save', function(next) {
+  this.avatar = `http://gravatar.com/avatar/${md5(this.username)}?d=identicon`
+  next()
+})
+
+// Hash password so it can't be seen w/ access to database
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err)
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err)
+  
+      this.password = hash
+      next()
+    })
+  })
+}) 
+
 
 const User = mongoose.model("User", UserSchema);
 
