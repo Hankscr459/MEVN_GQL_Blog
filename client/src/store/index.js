1a99010a1ef450c3c4f7435603bbb8811d76c9bb
Hankscr459 <defcw58@gmail.com>
2021-02-22 18:20:11 +0800

    Additional Config for ApolloClient, Send Token from LocalStorage

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -41,7 +41,8 @@ export default new Vuex.Store({
           variables: payload
         })
         .then(({ data }) => {
-          console.log(data.signinUser)
+          localStorage.setItem('token', data.signinUser.token)
+          // console.log(data.signinUser)
         })
         .catch(err => {
           console.log(err)
