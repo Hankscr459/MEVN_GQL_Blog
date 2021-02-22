efa0b84a4994fdb6d7e1bd05617ad1f14aa2d5bb
Hankscr459 <defcw58@gmail.com>
2021-02-22 17:23:49 +0800

    Add Signin Form, Write and Run signinUser Action, Return JWT

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import { GET_POSTS } from '../queries'
+import { GET_POSTS, SIGNIN_USER } from '../queries'
 import { defaultClient as apolloClient } from '../main'
 
 Vue.use(Vuex)
@@ -32,6 +32,19 @@ export default new Vuex.Store({
         .catch(err => {
           commit('setLoading', false)
           console.error(err)
+        })
+    },
+    signinUser: ({ commit }, payload) => {
+      apolloClient
+        .mutate({
+          mutation: SIGNIN_USER,
+          variables: payload
+        })
+        .then(({ data }) => {
+          console.log(data.signinUser)
+        })
+        .catch(err => {
+          console.log(err)
         })
     }
   },
