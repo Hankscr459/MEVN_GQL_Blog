c63376ed09da0f1d294cd6a079091ef7bc513fd4
Hankscr459 <defcw58@gmail.com>
2021-02-22 14:09:10 +0800

    Write and Run signinUser Mutation

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt'
+
 export default {
     Query: {
         getPosts: async (_, args, { Post }) => {
@@ -21,6 +23,19 @@ export default {
             }).save()
             return newPost
         },
+
+        signinUser: async (_, { username, password }, { User }) => {
+            const user = await User.findOne({ username })
+            if (!user) {
+                throw Error('User not found!')
+            }
+            const isValidPassword = await bcrypt.compare(password, user.password)
+            if (!isValidPassword) {
+                throw new Error('Invalid password')
+            }
+            return user
+        },
+
         signupUser: async (_, { username, email, password }, {User}) => {
             const user = await User.findOne({ username })
             if (user) {
