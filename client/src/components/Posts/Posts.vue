82c5c378d1e16430db3960bcbc4fe818ea9fe704
Hankscr459 <defcw58@gmail.com>
2021-02-24 16:28:32 +0800

    Add Grid Layout / Cards for Each Post in Posts Component

--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -1,24 +1,74 @@
 <template>
-  <v-container text-xs-center v-if="infiniteScrollPosts">
-    <div v-for="post in infiniteScrollPosts.posts" :key="post._id">
-      <img :src="post.imageUrl" height="100px">
-      <h3>{{post.title}}</h3>
-    </div>
-    <v-btn @click="showMorePosts" v-if="showMoreEnabled">Fetch More</v-btn>
+  <v-container fluid grid-list-xl>
+    <!-- Post Cards -->
+    <v-layout row wrap v-if="infiniteScrollPosts">
+      <v-flex xs12 sm6 v-for="post in infiniteScrollPosts.posts" :key="post._id">
+        <v-card hover>
+          <v-img :src="post.imageUrl" height="30vh" lazy></v-img>
+
+          <v-card-actions>
+            <v-card-title>
+              <div>
+                <div class="headline">{{post.title}}</div>
+                <span class="grey--text">{{post.likes}} likes - {{post.messages.length}} comments</span>
+              </div>
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn icon @click="showPostCreator = !showPostCreator">
+              <v-icon>{{`keyboard_arrow_${showPostCreator ? 'up' : 'down'}`}}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <!-- Post Creation title -->
+          <v-slide-y-transition>
+            <v-card-text v-show="showPostCreator" class="grey lighten-4">
+              <v-list-tile avatar>
+                <v-list-tile-avatar>
+                  <img :src="post.createdBy.avatar">
+                </v-list-tile-avatar>
+
+                <v-list-tile-content>
+                  <v-list-tile-title class="text--primary">{{post.createdBy.username}}</v-list-tile-title>
+                  <v-list-tile-sub-title class="font-weight-thin">Added {{post.createdDate}}
+                  </v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-action>
+                  <v-btn icon ripple>
+                    <v-icon color="grey lighten-1">info</v-icon>
+                  </v-btn>
+                </v-list-tile-action>
+              </v-list-tile>
+            </v-card-text>
+          </v-slide-y-transition>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <!-- Fetch More Button -->
+    <v-layout v-if="showMoreEnabled">
+      <v-flex xs12>
+        <v-layout justify-center row>
+          <v-btn color="info" @click="showMorePosts">Fetch More</v-btn>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+
   </v-container>
 </template>
 
 <script>
 import { INFINITE_SCROLL_POSTS } from '../../queries'
 
-const pageSize = 2
+const pageSize = 4
 
 export default {
   name: 'Posts',
   data () {
     return {
       pageNum: 1,
-      showMoreEnabled: true
+      showMoreEnabled: true,
+      showPostCreator: false
     }
   },
   apollo: {
