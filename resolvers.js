dafb393ea89d3d5858dd069008cc9dd883f3ed5a
Hankscr459 <defcw58@gmail.com>
2021-03-21 22:34:27 +0800

    Create updateUserPost Mutation

--- resolvers.js
+++ resolvers.js
@@ -89,6 +89,16 @@ export default {
             return newPost
         },
 
+        updateUserPost: async (_, { postId, userId, title, imageUrl, categories, description }, { Post }) => {
+            const post = await Post.findOneAndUpdate(
+                // Find post by postId and createdBy
+                { _id: postId, createdBy: userId },
+                { $set: { title, imageUrl, categories, description } },
+                { new: true }
+            )
+            return post
+        },
+
         addPostMessage: async (_, { messageBody, userId, postId }, { Post }) => {
             const newMessage = {
                 messageBody,
